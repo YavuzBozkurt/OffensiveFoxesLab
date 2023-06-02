d6f56755d52ad0e8ee96c6a2c1ac0700f2d8fe3c
Lars Hollander <lars@hollander-mail.com>
2023-06-02 11:01:15 +0200

    modified tool.py to work with python 2.7

--- tool.py
+++ tool.py
@@ -1,25 +1,31 @@
+import sys
 from arpPoison import *
+from dnsPoison import *
+
+def inputline():
+    sys.stdout.write(">>> ")
+    return 
 
 print("Welcome to the tool, this tool is a console style tool.")
 print("For the list of commands use the command 'help'")
 print("For more info on a command use the command 'help [command]'")
 print("to close or reset the tool, use the input 'ctrl+c'")
-print(">>>", end = " ")
+inputline()
 
 runner = True
 
 while runner:
-    user_in = input()
-    
+    user_in = raw_input()
+        
     if(user_in == "help"):
         print("Here is a list of commands you can use:")
         print("help\nhelp [command]\nmimattack\nonewaypoisoning")
-        print(">>>", end = " ")
+        inputline()
     
     elif(user_in == "help help"):
         print("'help' command is used to list all the commands available to user.")
         print("it can also be used in the format 'help [command]' to get more info on the specified command.")
-        print(">>>", end = " ")
+        inputline()
         
     elif(user_in == "mimattack"):
         mimAttack()
@@ -30,4 +36,4 @@ while runner:
     else:
         print("Command not found")
         print("For the list of commands use the command 'help'")
-        print(">>>", end = " ")
+        inputline()
